b8e57ae211deb9bbbdec3d8d9e01348216f8c099
Tinatin Samkharadze <tikosamkharadze11@gmail.com>
2025-07-27 22:33:05 +0400

    made some changes

--- src/test/java/ge/tbc/testautomation/LighthouseTests.java
+++ src/test/java/ge/tbc/testautomation/LighthouseTests.java
@@ -17,8 +17,6 @@ public class LighthouseTests extends BaseTest {
     public void testHomePagePerformance() {
         page.navigate(TBC_BANK_BASE_URL);
         String reportPath = LighthouseUtils.runPerformanceAudit(page.url(), HOME_PAGE_AUDIT);
-        Allure.addAttachment("Home Page Report", "text/html",
-                "Lighthouse report generated: lighthouse-reports/HomePageAudit-lighthouse.html");
         int performanceScore = getSpecificScore(reportPath, PERFORMANCE);
         int accessibilityScore = getSpecificScore(reportPath, ACCESSIBILITY);
         int bestPracticesScore = getSpecificScore(reportPath, BEST_PRACTICES);
@@ -47,17 +45,24 @@ public class LighthouseTests extends BaseTest {
     public void testTreasuryProductsPagePerformance() {
         page.navigate(TBC_BANK_TREASURY_PRODUCTS_PAGE_URL);
         String reportPath = LighthouseUtils.runPerformanceAudit(page.url(), TREASURY_PRODUCTS_PAGE_AUDIT);
-        Allure.addAttachment("Treasury Products Report", "text/html",
-                "Lighthouse report generated: lighthouse-reports/TreasuryProductsPageAudit-lighthouse.html");
         int performanceScore = getSpecificScore(reportPath, PERFORMANCE);
         int accessibilityScore = getSpecificScore(reportPath, ACCESSIBILITY);
         int bestPracticesScore = getSpecificScore(reportPath, BEST_PRACTICES);
         int seoScore = getSpecificScore(reportPath, SEO);
 
+        int fcp = getSpecificScore(reportPath, FIRST_CONTENTFUL_PAINT);
+        int lcp = getSpecificScore(reportPath, LARGEST_CONTENTFUL_PAINT);
+        int tti = getSpecificScore(reportPath, TIME_TO_INTERACTIVE);
+        double cls = getSpecificScore(reportPath, COMULATIVE_LAYOUT_SHIFT);
         softAssert.assertTrue(performanceScore >= MIN_PERFORMANCE_SCORE);
         softAssert.assertTrue(accessibilityScore > MIN_ACCESSIBILITY_SCORE);
         softAssert.assertTrue(bestPracticesScore > MIN_BEST_PRACTICES_SCORE);
         softAssert.assertTrue(seoScore > MIN_SEO_SCORE);
+
+        softAssert.assertTrue(fcp <= MAX_FCP_TIME);
+        softAssert.assertTrue(lcp <= MAX_LCP_TIME);
+        softAssert.assertTrue(tti <= MAX_TTI_TIME);
+        softAssert.assertTrue(cls <= MAX_CLS_VALUE);
 
     }
 
@@ -68,17 +73,24 @@ public class LighthouseTests extends BaseTest {
     public void testLoansPagePerformance() {
         page.navigate(TBC_BANK_LOANS_PAGE_URL);
         String reportPath = LighthouseUtils.runPerformanceAudit(page.url(), LOANS_PAGE_AUDIT);
-        Allure.addAttachment("Loans Page Report", "text/html",
-                "Lighthouse report generated: lighthouse-reports/LoansPageAudit-lighthouse.html");
         int performanceScore = getSpecificScore(reportPath, PERFORMANCE);
         int accessibilityScore = getSpecificScore(reportPath, ACCESSIBILITY);
         int bestPracticesScore = getSpecificScore(reportPath, BEST_PRACTICES);
         int seoScore = getSpecificScore(reportPath, SEO);
 
+        int fcp = getSpecificScore(reportPath, FIRST_CONTENTFUL_PAINT);
+        int lcp = getSpecificScore(reportPath, LARGEST_CONTENTFUL_PAINT);
+        int tti = getSpecificScore(reportPath, TIME_TO_INTERACTIVE);
+        double cls = getSpecificScore(reportPath, COMULATIVE_LAYOUT_SHIFT);
         softAssert.assertTrue(performanceScore >= MIN_PERFORMANCE_SCORE);
         softAssert.assertTrue(accessibilityScore > MIN_ACCESSIBILITY_SCORE);
         softAssert.assertTrue(bestPracticesScore > MIN_BEST_PRACTICES_SCORE);
         softAssert.assertTrue(seoScore > MIN_SEO_SCORE);
+
+        softAssert.assertTrue(fcp <= MAX_FCP_TIME);
+        softAssert.assertTrue(lcp <= MAX_LCP_TIME);
+        softAssert.assertTrue(tti <= MAX_TTI_TIME);
+        softAssert.assertTrue(cls <= MAX_CLS_VALUE);
 
     }
 
