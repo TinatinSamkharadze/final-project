87b832b7030f1ac44b54dfb45f15e9f69cb3d430
Tinatin Samkharadze <tikosamkharadze11@gmail.com>
2025-07-27 18:37:03 +0400

    fix tests

--- src/test/java/ge/tbc/testautomation/LighthouseTests.java
+++ src/test/java/ge/tbc/testautomation/LighthouseTests.java
@@ -1,17 +1,24 @@
 package ge.tbc.testautomation;
 
 import ge.tbc.testautomation.lighthouseutils.LighthouseUtils;
+import io.qameta.allure.*;
 import org.testng.annotations.Test;
 
 import static ge.tbc.testautomation.data.Constants.*;
 import static ge.tbc.testautomation.lighthouseutils.LighthouseUtils.getSpecificScore;
 
+@Epic("Performance Testing")
+@Feature("Lighthouse Audit Reports")
 public class LighthouseTests extends BaseTest {
 
+    @Description("Home Page Performance audit by lighthouse")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
     public void testHomePagePerformance() {
         page.navigate(TBC_BANK_BASE_URL);
         String reportPath = LighthouseUtils.runPerformanceAudit(page.url(), HOME_PAGE_AUDIT);
+        Allure.addAttachment("Home Page Report", "text/html",
+                "Lighthouse report generated: lighthouse-reports/HomePageAudit-lighthouse.html");
             int performanceScore = getSpecificScore(reportPath, PERFORMANCE);
             int accessibilityScore = getSpecificScore(reportPath, ACCESSIBILITY);
             int bestPracticesScore = getSpecificScore(reportPath, BEST_PRACTICES);
@@ -23,10 +30,16 @@ public class LighthouseTests extends BaseTest {
             softAssert.assertTrue(seoScore > MIN_SEO_SCORE);
 
     }
+
+
+    @Description("Treasury Products Page Performance audit by lighthouse")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
     public void testTreasuryProductsPagePerformance() {
         page.navigate(TBC_BANK_TREASURY_PRODUCTS_PAGE_URL);
         String reportPath = LighthouseUtils.runPerformanceAudit(page.url(), TREASURY_PRODUCTS_PAGE_AUDIT);
+        Allure.addAttachment("Treasury Products Report", "text/html",
+                "Lighthouse report generated: lighthouse-reports/TreasuryProductsPageAudit-lighthouse.html");
             int performanceScore = getSpecificScore(reportPath, PERFORMANCE);
             int accessibilityScore = getSpecificScore(reportPath, ACCESSIBILITY);
             int bestPracticesScore = getSpecificScore(reportPath, BEST_PRACTICES);
@@ -39,11 +52,15 @@ public class LighthouseTests extends BaseTest {
 
     }
 
+
+    @Description("Loans Page Performance audit by lighthouse")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
     public void testLoansPagePerformance() {
         page.navigate(TBC_BANK_LOANS_PAGE_URL);
         String reportPath = LighthouseUtils.runPerformanceAudit(page.url(), LOANS_PAGE_AUDIT);
-
+        Allure.addAttachment("Loans Page Report", "text/html",
+                "Lighthouse report generated: lighthouse-reports/LoansPageAudit-lighthouse.html");
             int performanceScore = getSpecificScore(reportPath, PERFORMANCE);
             int accessibilityScore = getSpecificScore(reportPath, ACCESSIBILITY);
             int bestPracticesScore = getSpecificScore(reportPath, BEST_PRACTICES);
